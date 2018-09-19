b4805066471df74282e355ca2d5cb07b0b632abf
tottaz <tzetter@thunderbeardesign.com>
2018-09-19 12:55:19 +0200

    Final version

--- cloud-function-iotWeather/functions/index.js
+++ cloud-function-iotWeather/functions/index.js
@@ -1,3 +1,5 @@
+
+// Set the variables
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 const bigquery = require('@google-cloud/bigquery')();
@@ -12,6 +14,7 @@ const db = admin.database();
  * Write raw data to bigquery
  * Maintain last data on firebase realtime database
  */
+
 exports.iotWeather = functions.pubsub
   .topic('iot-topic')
   .onPublish((message, context) => {
@@ -69,43 +72,3 @@ function insertIntoBigquery(data) {
   const table = dataset.table(functions.config().bigquery.tablename);
   return table.insert(data);
 }
-
-/**
- * Query bigquery with the last 7 days of data
- * HTTPS endpoint to be used by the webapp
- */
-exports.getReportData = functions.https.onRequest((req, res) => {
-  const projectId = process.env.GCLOUD_PROJECT;
-  const datasetName = functions.config().bigquery.datasetname;
-  const tableName = functions.config().bigquery.tablename;
-  const table = `${projectId}.${datasetName}.${tableName}`;
-
-  const query = `
-    SELECT 
-      TIMESTAMP_TRUNC(data.timestamp, HOUR, 'Europe/Amsterdam') data_hora,
-      avg(data.temp) as avg_temp,
-      avg(data.humidity) as avg_hum,
-      min(data.temp) as min_temp,
-      max(data.temp) as max_temp,
-      min(data.humidity) as min_hum,
-      max(data.humidity) as max_hum,
-      count(*) as data_points      
-    FROM \`${table}\` data
-    WHERE data.timestamp between timestamp_sub(current_timestamp, INTERVAL 7 DAY) and current_timestamp()
-    group by data_hora
-    order by data_hora
-  `;
-
-  return bigquery
-    .query({
-      query: query,
-      useLegacySql: false
-    })
-    .then(result => {
-      const rows = result[0];
-
-      cors(req, res, () => {
-        res.json(rows);
-      });
-    });
-});
